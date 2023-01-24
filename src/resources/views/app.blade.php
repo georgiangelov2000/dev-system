b16b6174b82ede12b0f5f606c635fdea27dba78f
georgiangelov2000 <angelov2000.abv.bg>
2023-01-25 00:56:53 +0200

    javascript-css-libraries

---
+++ src/resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link type="text/css" href="{{ asset('css/adminlte.min.css') }}" rel="stylesheet" />
+        <link type="text/css" href="{{ asset('css/all.min.css') }}" rel="stylesheet" />
+    </head>
+
+    <body class="sidebar-mini layout-fixed" style="height: auto;">
+        <div class='wrapper'> 
+        </div>
+    </body>
+
+    <script type="text/javascript" href="{{ asset('js/jquery.min.js') }}"></script>
+    <script type="text/javascript" href="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script type="text/javascript" href="{{ asset('js/adminlte.min.js') }}"></script>
+    <script type="text/javascript" href="{{ asset('js/jqueryui.min.js') }}"></script>
+
+</html>
