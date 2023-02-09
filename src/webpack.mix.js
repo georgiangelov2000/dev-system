2234008ae02cabec920a2753958aba60a2e3ca8b
georgiangelov2000 <angelov2000.abv.bg>
2023-02-09 15:03:06 +0200

    package-json-webpack

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -5,25 +5,26 @@ const fs = require('fs');
 const pathJs = './resources/js';
 const pathCss = './resources/sass';
 
+
 function discover(dir, type) {
-    let jsFiles = []
+    let jsFiles = [];
     fs.readdirSync(dir).forEach(file => {
-        let fileName = `${dir}/${file}`
+        let fileName = `${dir}/${file}`;
         if(fs.statSync(fileName).isFile()) {
             if (fileName.endsWith(type)) {
-                jsFiles.push(fileName)
+                jsFiles.push(fileName);
             }
         } else {
-            jsFiles = jsFiles.concat(discover(fileName, type))
+            jsFiles = jsFiles.concat(discover(fileName, type));
         }
-    })
-    return jsFiles
-}
+    });
+    return jsFiles;
+};
 
 discover(pathJs, '.js').forEach(file => {
     mix.js(file, 'public/js')
-})
+});
 
 discover(pathCss, '.scss').forEach(file => {
     mix.sass(file, 'public/css')
-})
+});
